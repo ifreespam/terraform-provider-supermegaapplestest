5b78f234688b3a291d37af2c55f1bc2da83251cf
Valentin Valentinovich <ifreespam@gmail.com>
2021-07-20 20:55:10 +0300

    fix workflow ci 7

--- supermegaapplestest/provider_test.go
+++ supermegaapplestest/provider_test.go
@@ -4,17 +4,22 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestAccFirst(t *testing.T) {
 	skipTestIfNoTFAccFlag(t)
 	testRandomEnvAvailable(t)
+	fmt.Println("[DEBUG] TestAccFirst start")
+	time.Sleep(time.Second * 20)
 	fmt.Println("[DEBUG] TestAccFirst done")
 }
 
 func TestAccSecond(t *testing.T) {
 	skipTestIfNoTFAccFlag(t)
 	testRandomEnvAvailable(t)
+	fmt.Println("[DEBUG] TestAccSecond start")
+	time.Sleep(time.Second * 20)
 	fmt.Println("[DEBUG] TestAccSecond done")
 }
 
