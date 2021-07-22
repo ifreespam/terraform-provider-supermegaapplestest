022ec316872538982f8b93e851dd21d636c6a9cb
Valentin Valentinovich <ifreespam@gmail.com>
2021-07-22 16:33:13 +0300

    fix4

--- supermegaapplestest/provider_test.go
+++ supermegaapplestest/provider_test.go
@@ -19,7 +19,7 @@ func TestAccSecond(t *testing.T) {
 	skipTestIfNoTFAccFlag(t)
 	testRandomEnvAvailable(t)
 	fmt.Println("[DEBUG] TestAccSecond start")
-	time.Sleep(time.Second * 31)
+	time.Sleep(time.Second * 30)
 	fmt.Println("[DEBUG] TestAccSecond done")
 }
 
